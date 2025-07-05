eee9476e57c344e6805b9f044bfdbdc13f1f3957
fiskasyela <fiskasyela@gmail.com>
2025-07-05 13:01:16 +0700

    fix: layout on chat

--- app/(chat)/layout.tsx
+++ app/(chat)/layout.tsx
@@ -8,10 +8,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`antialiased bg-pink-200 h-screen overflow-x-hidden`}>
-        <LoadingProvider>{children}</LoadingProvider>
-      </body>
-    </html>
+    <div className={`antialiased bg-pink-200 h-screen overflow-x-hidden`}>
+      <LoadingProvider>{children}</LoadingProvider>
+    </div>
   );
 }
