0e7a544691e64e4b6311f3261fc350b1e9e10324
gentakurniawan <mgentakurniawan6@gmail.com>
2025-07-10 10:54:20 +0700

    refactor(button): update button component styling

--- components/global/button.tsx
+++ components/global/button.tsx
@@ -3,14 +3,27 @@ import React from "react";
 type ButtonProps = {
   className?: string;
   children?: React.ReactNode;
+  disabled?: boolean;
+  size?: "small" | "large";
   onClick?: () => void;
 };
 
-export default function Button({ className, children, onClick }: ButtonProps) {
+export default function Button({
+  className,
+  children,
+  disabled,
+  size = "large",
+  onClick,
+}: ButtonProps) {
   return (
     <button
-      className={`pixel-corners bg-pink-200 pixel-corners cursor-pointer hover:bg-pink-300 hover:scale-105 duration-100 ${className}`}
+      className={`${
+        size === "large"
+          ? "pixel-corners text-2xl font-bold"
+          : "pixel-corners-small text-lg font-bold"
+      } flex flex-row justify-center items-center gap-2 bg-pink-200 cursor-pointer text-pink-500 hover:scale-105 duration-100 ${className} disabled:opacity-50 disabled:cursor-not-allowed`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
