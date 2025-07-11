ca01a971adfbfd2d8d6fe5a2bd0d16a60923355b
gentakurniawan <mgentakurniawan6@gmail.com>
2025-07-11 16:19:12 +0700

    style(chat): refine header and layout styles in ChatPage component

--- app/(chat)/chat/page.tsx
+++ app/(chat)/chat/page.tsx
@@ -285,8 +285,8 @@ export default function ChatPage() {
   };
 
   return (
-    <div className="p-[2.5%] max-md:pt-0 w-screen max-md:min-h-screen max-md:h-full h-screen">
-      <header className="h-fit flex justify-between md:rounded-lg px-4 py-2 md:p-4 bg-shadow-primary md:mb-6 max-md:fixed z-10 max-md:left-0 max-md:top-0 w-full">
+    <div className="p-[2.5%] w-screen max-md:min-h-screen max-md:h-full h-screen">
+      <header className="h-fit flex justify-between rounded-lg px-4 py-2 md:p-4 bg-shadow-primary mb-6 w-full">
         <article className="flex gap-2 items-center">
           <span className="font-chango font-extrabold text-pink-500 text-xl">
             Lovey
@@ -316,7 +316,7 @@ export default function ChatPage() {
           />
         </div>
       </header>
-      <div className="grid md:grid-cols-3 grid-cols-1 gap-y-6 md:gap-x-6 md:h-[calc(100vh-9rem)] h-full max-md:mt-14">
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-y-6 md:gap-x-6 md:h-[calc(100vh-9rem)] h-full">
         <main
           className={`col-span-2 h-full bg-shadow-primary rounded-lg px-2.5 py-2.5 md:px-6 md:py-6 flex flex-col justify-between gap-2 w-full md:overflow-y-auto`}
         >
